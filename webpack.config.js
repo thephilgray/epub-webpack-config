11909a3262e6b8c80af6060ecda4250eb72c255b
Phillip Gray <phillipgray@Phillips-iMac.local>
2019-02-12 08:59:47 -0500

    handle basic spine and metadata processing from assets, add todos

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { xml2js, js2xml } = require('xml-js');
 const chalk = require('chalk');
-const { xml2json, json2xml } = require('xml-js');
 
 const { log } = console;
 
@@ -27,71 +27,81 @@ const exts = {
 };
 
 class EpubPlugin {
+  // eslint-disable-next-line
   apply(compiler) {
     compiler.hooks.done.tap('EPUB Plugin', (
       stats /* stats is passed as argument when done hook is tapped.  */
     ) => {
-      fs.unlinkSync('./dist/out.js');
-      log(chalk.red('delete dist/out.js'));
-      const assets = stats.compilation.assets;
-      const assetMap = Object.keys(assets).map(asset => {
+      const OPF_PATH = './src/content.opf';
+      const XML_JS_OPTIONS = {
+        spaces: 2,
+        compact: true,
+        ignoreComment: true,
+      };
+      const PAGES_EXTENSION = 'xhtml';
+      // Get data from source opf
+      const xmlFromOpf = fs.readFileSync(OPF_PATH, 'utf8');
+      const jsFromOpf = xml2js(xmlFromOpf, XML_JS_OPTIONS);
+
+      // const manifestItemFromOpf = jsFromOpf.package.manifest;
+      // const spineFromOpf = jsFromOpf.package.spine.itemref;
+
+      const { assets } = stats.compilation;
+
+      /**
+       * TODO: Create an api for merging updates from assets
+       *
+       *  1. remove: filter out and log any src opf items that no longer exist in assets
+       *  2. add: log and add new assets which were not found in the original opf
+       *  3. update: when an item is found to exist in both the src opf and the assets, log and merge it
+       *  4. getUpdatedOpf: return the updated opf object
+       *
+       * */
+
+      //  TODO: merge metadata from a user-supplied config
+
+      // TODO: handle properties
+      // TODO: allow this list to be ordered and refined by a user-supplied config
+      const manifestItemFromAssets = Object.keys(assets).map(asset => {
         const assetObj = assets[asset].existsAt;
         const { root, dir, base, ext, name } = path.parse(assetObj);
         return {
-          href: path.relative('./dist/', assetObj),
-          id: name,
-          'media-type': exts[ext.substr(1)],
+          _attributes: {
+            href: path.relative('./dist/', assetObj),
+            id: name,
+            'media-type': exts[ext.substr(1)],
+          },
         };
       });
 
-      // log(assetMap);
-
-      // old assets
-      const xml = fs.readFileSync('./src/content.opf', 'utf8');
-      const json = xml2json(xml, { spaces: 2, compact: true });
-      const parsedJson = JSON.parse(json);
-      const manifest = parsedJson.package.manifest.item;
-
-      // log(manifest);
-
-      // old spine
-      const spine = parsedJson.package.spine.itemref;
-      log(spine);
-      // // log(spine);
-
+      // TODO: handle properties like linear
       // for now just get all xhtml pages
-      const spinePages = assetMap
-        .filter(asset => asset['media-type'] === 'application/xhtml+xml')
-        .map(item => ({ _attributes: { idref: item.id } }));
+      // TODO: allow this list to be ordered and refined by a user-supplied config
+      const spineFromAssets = manifestItemFromAssets
+        .filter(
+          ({ _attributes }) =>
+            _attributes['media-type'] === exts[PAGES_EXTENSION]
+        )
+        .map(({ _attributes }) => ({ _attributes: { idref: _attributes.id } }));
 
-      log(spinePages);
+      // merge manifestItemFromAssets and spineFromAssets into the the new opf
+      const updatedOpf = {
+        ...jsFromOpf,
+        package: {
+          ...jsFromOpf.package,
+          manifest: {
+            item: manifestItemFromAssets,
+          },
+          spine: {
+            ...jsFromOpf.package.spine,
+            itemref: spineFromAssets,
+          },
+        },
+      };
 
-      // update old assets with new assets
+      const updatedOpfXml = js2xml(updatedOpf, XML_JS_OPTIONS);
 
-      // const updatedJson = {
-      //   ...json,
-      //   package: {
-      //     ...json.package,
-      //     spine: { toc: "toc", itemref: { ...spinePages } },
-      //     manifest: { item: assetMap }
-      //   }
-      // };
-      // log(updatedJson);
-      // const updatedXml = json2xml(updatedJson);
-
-      // log(updatedJson.package.manifest.item);
-
-      // fs.writeFileSync("./dist/content.opf", updatedXml);
-      // or use a template
-      // ejs.renderFile(
-      //   "./templates/content.ejs",
-      //   { items: assetMap, pages: spinePages },
-      //   {},
-      //   (err, str) => {
-      //     if (err) return log(err);
-      //     fs.writeFileSync("./dist/content.opf", str);
-      //   }
-      // );
+      fs.writeFileSync('./dist/content.opf', updatedOpfXml);
     });
   }
 }
